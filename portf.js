af063297da9787f0fbbbcfbbbb4989b40f07c51c
MOHAMMED <sowamohamed71@gmail.com>
2022-09-27 18:30:53 +0200

    first commit

---
+++ portf.js
@@ -0,0 +1,16 @@
+
+// const myfun = document.getElementsByTagName('div')
+// console.log(myfun[1]);
+// window.localStorage.setItem('color', 'red');
+// document.st
+
+
+
+
+/*const sleepTimer = (alarm) => {
+    console.log('My alarm is set for: ' + alarm);
+  }
+   
+  sleepTimer('8:30AM');
+}
+*/
